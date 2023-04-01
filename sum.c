cc4d6af5f2a5062c302ef8354ad8a3e4c9b7c2a2
Your Nam~ <abdulkhalekj03@gmail.com>
2023-04-01 16:15:18 +0600

    setup

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+      int a,b;
+    printf("enter the number:");
+    scanf("%d",&a);
+    printf("enter the number :");
+    scanf("%d",&b); 
+    if a>b {
+        printf ("A is largest number:",);
+    }
+    else{
+        printf("B is largest number :",);
+
+    }
+    return 0 ;
+}
